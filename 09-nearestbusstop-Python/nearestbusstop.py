8e571f166eae3c0c66ff4b9e40f82e7d124fc84a
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-12 17:37:23 +0530

    D3-1

--- 09-nearestbusstop-Python/nearestbusstop.py
+++ 09-nearestbusstop-Python/nearestbusstop.py
@@ -6,6 +6,13 @@
 # and the nearest bus stop to 13 street is 16th street.
 
 
-
 def fun_nearestbusstop(street):
-	return -1
+	a=street//8
+	n1=8*a
+	n2=8*(a+1)
+	s1=abs(n1-street)
+	s2=abs(n2-street)
+	if(s1>s2):
+    		return n2
+	else:
+    		return n1
