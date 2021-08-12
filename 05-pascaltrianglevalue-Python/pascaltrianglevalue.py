cb91f13a45a1148f70f8e7415ff728d03d739860
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-12 19:26:54 +0530

    d2_5

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -6,7 +6,33 @@
 # are not legal values, return None, instead of crashing. 
 
 
+def printPascal(n):
+        matrix=[]
+        for line in range(0, n):
+                a=[]
+                for i in range(0, line + 1):
+                        a.append(binomialCoeff(line, i))
+                matrix.append(a)
+        return matrix
+     
+def binomialCoeff(n, k) :
+    res = 1
+    if (k > n - k) :
+        k = n - k
+    for i in range(0 , k) :
+        res = res * (n - i)
+        res = res // (i + 1)
+     
+    return res
+
 
 
 def fun_pascaltrianglevalue(row, col):
-	return 1
+	a=printPascal(row+1)
+	for i in range(len(a)):
+		for j in range(i+1):
+			if(i==row and j==col):
+				return a[i][j]
+			else:
+				continue
+	return 0
