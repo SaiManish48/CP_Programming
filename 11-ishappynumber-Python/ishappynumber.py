d59a0f1665b6c9338080d69df05c98bd512cbacc
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-21 23:50:38 +0530

    happy number module 8

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -17,6 +17,24 @@
 # assert(ishappynumber(404) == True)
 # assert(ishappynumber(405) == False)
 
+def sumofsquares(n):
+        sumofsquare=0
+        while(n):
+                sumofsquare=sumofsquare+((n%10)*(n%10))
+                n=int(n/10)
+        return sumofsquare
+
 def ishappynumber(n):
-	# your code goes here
-	pass
+        strt=n
+        stp=n
+        while(True):
+                strt=sumofsquares(strt)
+                stp=sumofsquares(sumofsquares(stp))
+                if(strt!=stp):
+                        continue;
+                else:
+                        break;
+        if(strt==1):
+                return True
+        else:
+                return False
