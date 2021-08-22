1640c773dbde173218105227586b5fced3d052fd
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-22 23:06:24 +0530

    linked list

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,14 +19,28 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
+        f=self.head
+        if(self.head):
+            while(f.next):
+                f=f.next
+            f.next=new_element
+        else:
+            self.head=new_element
             
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        crnt=self.head
+        cnt=1
+        if(position<1):
+            return None
+        while(crnt and cnt<=position):
+            if(cnt==position):
+                return crnt
+            crnt=crnt.next
+            cnt +=1
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +48,31 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
-    
+        count=1
+        current=self.head
+        if(position<1):
+            return None
+        elif(position==1):
+            new_element.next=self.head
+            self.head=new_element
+        else:
+            while(current and count<position):
+                if(count==position-1):
+                    new_element.next=current.next
+                    current.next=new_element
+                current=current.next
+                count+=1
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        current=self.head
+        before=None
+        while(current.value!=value and current.next):
+            before=current
+            current=current.next
+        if(current.value==value):
+            if(before):
+                before.next=current.next
+            else:
+                self.head=current.next
