1da2f0df16a5516b772b9aeec01067562e53c015
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-23 13:19:12 +0530

    magic square 11

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -7,4 +7,69 @@
 
 
 def fixmostlymagicsquare(L):
-	# Your code goes here
+        M=L
+        sumofrow=[]
+        for i in range(len(M)):
+                sum=0
+                for j in range(len(M[0])):
+                        sum+=M[i][j]
+                sumofrow.append(sum)
+        rows=sumofrow.count(sumofrow[0])==len(sumofrow)
+        print(sumofrow,len(sumofrow),rows)
+        if(rows):
+                #column
+                sumofcol=[]
+                for i in range(len(M[0])):
+                        sum=0
+                        for j in range(len(M)):
+                                sum+=M[j][i]
+                        sumofcol.append(sum)
+                        # print(sumofrow,sumofcol)
+                cols=sumofcol.count(sumofcol[0])==len(sumofcol)
+                if(cols):
+                        #diagonal
+                        d1=0
+                        d2=0
+                        for i in range(len(M)):
+                                d1+=M[i][i]
+                                d2+=M[i][-1-i]
+                        if(d1!=d2):
+                                return True
+
+        else:
+                rowdict=dict((i,sumofrow.count(i)) for i in sumofrow)
+                rwrong=min(rowdict,key=rowdict.get)
+                rcorrect=max(rowdict,key=rowdict.get)
+                diffr=rwrong-rcorrect
+                for i in range(len(sumofrow)):
+                        if(sumofrow[i]==rwrong):
+                                flag1=i
+                                #column
+                sumofcol=[]
+                for i in range(len(M[0])):
+                        sum=0
+                        for j in range(len(M)):
+                                sum+=M[j][i]
+                        sumofcol.append(sum)
+                # print(sumofrow,sumofcol)
+                cols=sumofcol.count(sumofcol[0])==len(sumofcol)
+                if(cols):
+                        #diagonal
+                        d1=0
+                        d2=0
+                        for i in range(len(M)):
+                                d1+=M[i][i]
+                                d2+=M[i][-1-i]
+                        if(d1!=d2):
+                                return True
+                else:
+                        coldict=dict((i,sumofcol.count(i)) for i in sumofcol)
+                        cwrong=min(coldict,key=rowdict.get)
+                        ccorrect=max(coldict,key=rowdict.get)
+                        diffc=cwrong-ccorrect
+                        for i in range(len(sumofcol)):
+                                if(sumofcol[i]==cwrong):
+                                        flag2=i
+                        if(diffr==diffc):
+                                M[flag1][flag2]=M[flag1][flag2]-diffr
+        return M
