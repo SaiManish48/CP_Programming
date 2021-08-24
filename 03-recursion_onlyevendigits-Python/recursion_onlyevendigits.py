5046b975c904a345d94ad72e930ae73f85e75fb6
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-24 11:48:26 +0530

    recusrion only even digit

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,20 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
-		return []
+def iseven(n,i=0,null=0):
+        if(n==0):
+                return null
+        tmp=n%10
+        if(tmp%2==0):
+                null +=(10**i*tmp)
+                i +=1
+        return iseven(n//10,i,null)
+
+def fun_recursion_onlyevendigits(l):
+        lst=[]
+        if(len(l)==0):
+                return lst
+        else:
+                evn=iseven(l[0])
+                lst.append(evn)
+                return lst+fun_recursion_onlyevendigits(l[1:])
