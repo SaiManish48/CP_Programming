90a480fb1753586fd3eb35416d6849f5fc24c373
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-28 20:53:00 +0530

    property 309

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -4,6 +4,21 @@
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
 
+def proprty(n):
+        num='0123456789'
+        ans=str(n**5)
+        for i in num:
+                if i not in ans:
+                        return False
+        return True
+
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+        fnd=0
+        guess=308
+        while(fnd<=n):
+                guess +=1
+                if(proprty(guess)):
+                        fnd +=1
+        return guess
+
+print(nthwithproperty309(0))
