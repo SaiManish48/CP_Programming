4ee5553a4ff142ec1638a393c336286186c30b19
SaiManish48 <72133265+SaiManish48@users.noreply.github.com>
2021-08-28 19:44:42 +0530

    longest common substring

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -9,4 +9,16 @@
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    pass
+    fnl=""
+    l1=len(s2)
+    l2=len(s1)
+    for i in range(l1):
+        smlar=''
+        for j in range(l2):
+            if(i+j<l1 and s2[i+j]==s1[j]):
+                smlar+=s1[j]
+            else:
+                if(len(smlar)>=len(fnl)):
+                    fnl=smlar
+                    smlar=""
+    return fnl
